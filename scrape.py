b3f393069bf2953057a3c4865db0d7f6146c2f17
cjannun <93235878+cjannun@users.noreply.github.com>
2022-11-05 15:38:35 -0400

    Scroll 1.0

--- scrape.py
+++ scrape.py
@@ -1,9 +1,13 @@
 import time
+import os
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
 
 from selenium.webdriver.common.by import By
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #tutorial: https://www.scrapingbee.com/blog/selenium-python/
 
@@ -11,12 +15,14 @@ from selenium.webdriver.common.by import By
 #options.headless = False
 #options.add_argument("--window-size=1920,1200")
 
-DRIVER_PATH = 'C:/Users/Yump0/Desktop/SCRAPER4/chromedriver.exe'
+driverPath = os.path.abspath('chromedriver.exe')
+DRIVER_PATH = driverPath
+
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 driver.get('https://open.spotify.com/playlist/6C9GNbtKzpnqxCZjZ9RtSp')
 #driver.find_element(By.CLASS_NAME, 'class="Type__TypeElement-goli3j-0 dlOSsY t_yrXoUO3qGsJS4Y6iXX standalone-ellipsis-one-line')
 time.sleep(3)
-songs = driver.find_elements(By.TAG_NAME, 'div')
+
 #songs = driver.find_elements(By.CLASS_NAME, "Type__TypeElement-goli3j-0 dlOSsY t_yrXoUO3qGsJS4Y6iXX standalone-ellipsis-one-line")
 #print(songs)
 
@@ -27,7 +33,20 @@ songs = driver.find_elements(By.TAG_NAME, 'div')
 # page = driver.find_element_by_xpath('/html')
 # driver.execute_script("""arguments[0].scrollTo(0, arguments[0].scrollHeight);""", page)
 
-fileToWrite = open("C:/Users/Yump0/Desktop/scraper5/divs.txt", "w")
+fileToWrite = open("divs.txt", "w")
+
+bottom_sentinel = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@data-testid='bottom-sentinel']")))
+song_list = []
+for x in range(10):
+    songs = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-testid='tracklist-row']")))
+    for value in songs:
+        song_list.append(value.get_attribute('innerHTML'))
+    time.sleep(0.5)
+    bottom_sentinel.location_once_scrolled_into_view
+    driver.implicitly_wait(15)
+
+songs = driver.find_elements(By.TAG_NAME, 'div')
+
 i = 0
 for value in songs:
     fileToWrite.write(value.text)
