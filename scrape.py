1f5f6fc957e12998334957998faa145cc86d0731
cjannun <93235878+cjannun@users.noreply.github.com>
2022-11-05 16:39:46 -0400

    Scroll 1.1

--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import time
+import os
 from selenium import webdriver
 
 from selenium.webdriver.chrome.options import Options
@@ -20,7 +21,7 @@ driverPath = os.path.abspath('chromedriver.exe')
 DRIVER_PATH = driverPath
 
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get('https://open.spotify.com/playlist/6C9GNbtKzpnqxCZjZ9RtSp')
+driver.get('https://open.spotify.com/playlist/10t1Tq0wPjXMDyLD0jGFrN?si=9086e4a954b04194&nd=1')
 #driver.find_element(By.CLASS_NAME, 'class="Type__TypeElement-goli3j-0 dlOSsY t_yrXoUO3qGsJS4Y6iXX standalone-ellipsis-one-line')
 time.sleep(3)
 
@@ -38,25 +39,30 @@ fileToWrite = open("divs.txt", "w")
 
 bottom_sentinel = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "//div[@data-testid='bottom-sentinel']")))
 song_list = []
-for x in range(10):
+
+songs_element = driver.find_element(By.XPATH, "//*[@id='main']/div/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div[2]/main/div/section/div[1]/div[5]/div/span[2]").text
+
+song_num = songs_element.split()[0].replace(',', '')
+
+while (len(song_list) < int(song_num)):
     songs = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@data-testid='tracklist-row']")))
-    for value in songs:
-        song_list.append(value.get_attribute('innerHTML'))
+    songs = driver.find_elements(By.TAG_NAME, 'div')
+    print(songs[-1].text)
     time.sleep(0.5)
     bottom_sentinel.location_once_scrolled_into_view
     driver.implicitly_wait(15)
 
-songs = driver.find_elements(By.TAG_NAME, 'div')
+print(len(song_list))
 
-i = 0
-for value in songs:
-    fileToWrite.write(value.text)
-    print(value.text)
-    i = 1 + i
-    if(i == 3):
-        break
-    print(i)
-fileToWrite.close()
+# i = 0
+# for value in songs:
+#     fileToWrite.write(value.text)
+#     print(value.text)
+#     i = 1 + i
+#     if(i == 3):
+#         break
+#     print(i)
+# fileToWrite.close()
 
-print("DONE")
-driver.quit
+# print("DONE")
+# driver.quit
